3e3f7ea73f2fe1b5eec3e973445fa9779ebdb8a9
Bob Ippolito <bob@redivi.com>
2025-09-24 16:18:13 -0700

    add country code db

--- src/salesforce/twoLetterCountryCode.ts
+++ src/salesforce/twoLetterCountryCode.ts
@@ -1,10 +1,13 @@
+import { whereAlpha3 } from "iso-3166-1";
+
 export function twoLetterCountryCode(
   country: string | null | undefined,
 ): string | null {
-  if (country?.length === 2) {
-    return country;
-  } else if (country === "USA") {
-    return "US";
+  if (!country) {
+    return null;
+  } else if (country.length === 2) {
+    return country.toUpperCase();
   }
-  return null;
+  const c = whereAlpha3(country);
+  return c ? c.alpha2 : null;
 }
